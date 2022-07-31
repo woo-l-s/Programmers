738299bf1161f5ee12994f1ed10ac118c3ca413f
jin <tjrdn110@naver.com>
2022-07-31 11:15:30 +0900

    220731

---
+++ 김서방은 1에 있다.py
@@ -0,0 +1,15 @@
+# 람다함수의 예시
+# (lambda x,y: x + y)(10, 20)
+
+def solution(s):
+    answer = "김서방은 " + str(s.index('Kim')) + "에 있다"
+    return answer
+
+
+seoul = ["Jane", "Kim"]
+
+r = solution(seoul)
+print(r)
+
+
+
